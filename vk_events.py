6e4dd6480e7a0a0114d1a4d97bea2735dde6fa6c
Stulov Tikhon <stulov@safit.pro>
2024-01-26 19:30:46 +0300

    kate mobile

--- vk_events.py
+++ vk_events.py
@@ -1,34 +1,21 @@
 # -*- coding: utf-8 -*-
 
-from getpass import getpass
 import vk_api
 
-# При двухфакторной аутентификации вызывается эта функция
-def auth_handler():
-    key = input("Enter authentication code: ")
-    # Если: True - сохранить, False - не сохранять.
-    remember_device = True
-
-    return key, remember_device
+app_id = 2685278 # KateMobile
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
 
     parser = ArgumentParser(prog='vk event', description='VK events to Google calenar')
-
-    parser.add_argument('-l', '--login', required=True, type=str, metavar='<login>', help='VK user login or email')
-    parser.add_argument('-p', '--pass', dest = 'password', type=str, metavar='<password>', help='VK user password')
-
+    parser.add_argument('-t', '--token', required=True, type=str, metavar='<token>', help='VK user token. see https://oauth.vk.com/authorize?client_id=2685278&scope=2199023255551&redirect_uri=https://api.vk.com/blank.html&display=page&response_type=token&revoke=1')
     args = parser.parse_args()
-    if not args.password:
-        args.password = getpass('input password:')
-    
-    try:
-        vk_session = vk_api.VkApi(args.login, args.password)
-    except:
-        vk_session = vk_api.VkApi(args.login, args.password, auth_handler = auth_handler)
-    vk_session.auth()
+
+    vk_session = vk_api.VkApi(login = ' ', token = args.token, app_id = app_id)
+    vk_session.auth(token_only = True)
 
     vk = vk_session.get_api()
 
-    print(vk)
+    responce = vk.users.get()
+
+    print(responce)
