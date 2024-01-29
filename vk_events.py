c74d5ae2e16c74d6ed355058a28eeb31acb68559
Stulov Tikhon <stulov@safit.pro>
2024-01-29 15:16:36 +0300

    get active events success

--- vk_events.py
+++ vk_events.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import vk_api
+import requests
+import time
 
 app_id = 2685278 # KateMobile
 
@@ -8,14 +9,21 @@ if __name__ == "__main__":
     from argparse import ArgumentParser
 
     parser = ArgumentParser(prog='vk event', description='VK events to Google calenar')
-    parser.add_argument('-t', '--token', required=True, type=str, metavar='<token>', help='VK user token. see https://oauth.vk.com/authorize?client_id=2685278&scope=2199023255551&redirect_uri=https://api.vk.com/blank.html&display=page&response_type=token&revoke=1')
+    parser.add_argument('-t', '--token', required=True, type=str, metavar='<token>', help='VK user token. see https://oauth.vk.com/authorize?client_id=2685278&scope=offline&redirect_uri=https://api.vk.com/blank.html&response_type=token')
     args = parser.parse_args()
 
-    vk_session = vk_api.VkApi(login = ' ', token = args.token, app_id = app_id)
-    vk_session.auth(token_only = True)
+    data = {'access_token': args.token, 'v': '5.199'}
+ 
+    response = requests.post('https://api.vk.com/method/users.get', data = data)
+    if not response: exit(-1)
+    print(response.json())
+    user_id = response.json()['response'][0]['id']
 
-    vk = vk_session.get_api()
-
-    responce = vk.users.get()
-
-    print(responce)
+    data['user_id'] = user_id
+    data['filter'] = 'events'
+    data['extended'] = 1
+    data['fields'] = 'start_date,finish_date,addresses'
+    response = requests.post('https://api.vk.com/method/groups.get', data = data)
+    if not response: exit(-1)
+    events = [{k: v for k, v in i.items() if not 'photo' in k} for i in response.json()['response']['items'] if i['start_date'] > time.time() - 2 * 60 * 60]
+    print(events)
