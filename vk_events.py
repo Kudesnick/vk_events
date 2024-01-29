06bcf76064a7abd719020823bc9e8b446cffa25b
Stulov Tikhon <stulov@safit.pro>
2024-01-29 19:01:34 +0300

    refactoring

--- vk_events.py
+++ vk_events.py
@@ -2,18 +2,20 @@
 
 import requests
 import time
+from pathlib import Path
 
 app_id = 2685278 # KateMobile
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
 
-    parser = ArgumentParser(prog='vk event', description='VK events to Google calenar')
-    parser.add_argument('-t', '--token', required=True, type=str, metavar='<token>', help='VK user token. see https://oauth.vk.com/authorize?client_id=2685278&scope=offline&redirect_uri=https://api.vk.com/blank.html&response_type=token')
+    parser = ArgumentParser(prog='vk events', description='VK events to Google calenar')
+    parser.add_argument('-vt', '--vk-token', dest = 'vk', required=True, type=str, metavar='<vk_token>', help='VK user token. see https://oauth.vk.com/authorize?client_id=2685278&scope=offline&redirect_uri=https://api.vk.com/blank.html&response_type=token')
     args = parser.parse_args()
+    vk_token = args.vk if not Path(args.vk).is_file() else Path(args.vk).read_text()
 
-    data = {'access_token': args.token, 'v': '5.199'}
- 
+    data = {'access_token': vk_token, 'v': '5.199'}
+
     response = requests.post('https://api.vk.com/method/users.get', data = data)
     if not response: exit(-1)
     print(response.json())
